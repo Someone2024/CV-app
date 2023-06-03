43b6fb5ded212ec4d7f8c1c1f56720e66f4829f4
Someone <bassilsantiago@gmail.com>
2023-06-02 20:29:11 -0500

    defined the deploy configuration

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: '/',
+  }
+
+  if (command !== 'serve') {
+    config.base = 'https://github.com/Someone2024/CV-app'
+  }
+
+  return config
 })
+
+
