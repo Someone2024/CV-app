2e98464bbe147c1e97f4e5c4423aaa01a6570b38
Someone <bassilsantiago@gmail.com>
2023-06-02 20:52:23 -0500

    new config

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig(({ command }) => {
   const config = {
     plugins: [react()],
-    base: '/https://github.com/Someone2024/CV-app',
+    base: '/CV-app/',
   }
 
   return config
