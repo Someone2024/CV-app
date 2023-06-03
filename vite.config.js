8866041d0b58d42ffeb78a222fcb6ac7b3f61c7b
Someone <bassilsantiago@gmail.com>
2023-06-02 21:11:45 -0500

    modify base property

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig(({ command }) => {
   const config = {
     plugins: [react()],
-    base: '/CV-app/',
+    base: '/',
   }
 
   return config
